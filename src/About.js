260af4b0ece25c818a217697ed269da6be0f366f
Curial-20 <zachattack3307@icloud.com>
2025-03-03 13:49:41 -0500

    Updated About.js

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ const About = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Learn more about us here.</p>
+      <p>Our goal at PCPicker is to provide the user with information on how to plan, research, and build a PC. With the help of our currated lists of parts based on your preferences and answers to our quiz questions.</p>
+    // https://quizlet.com/791008659/comp380-final-review-multiple-choice-questions-flash-cards/
     </div>
   );
 };

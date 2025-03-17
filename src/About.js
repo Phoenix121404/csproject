e8a4fc36818b9f488da82767c9e2987dff5d32f5
Curial-20 <zachattack3307@icloud.com>
2025-03-17 15:38:57 -0400

    Firebase Initialization
    Added Firebase and the required dependencies. Firebase code was added into Firebase.js, methods for both authentication and firestore functionality will be added in a later commit.

--- src/About.js
+++ src/About.js
@@ -5,7 +5,6 @@ const About = () => {
     <div>
       <h1>About Us</h1>
       <p>Our goal at PCPicker is to provide the user with information on how to plan, research, and build a PC. With the help of our currated lists of parts based on your preferences and answers to our quiz questions.</p>
-    // https://quizlet.com/791008659/comp380-final-review-multiple-choice-questions-flash-cards/
     </div>
   );
 };
